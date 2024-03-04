4b2abb4103d6d46eb8f2b28123e91c46947fa8b5
https://thamudhifernando@gitlab.com <fernandothamudhi@gmail.com>
2024-03-04 19:52:31 +0530

    Writing a Failing Test Case in TDD

---
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -0,0 +1,13 @@
+package example.cashcard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CashCardJsonTest {
+    @Test
+    void myFirstTest(){
+        assertThat(1).isEqualTo(42);
+        assertThat(42).isEqualTo(42);
+    }
+}
