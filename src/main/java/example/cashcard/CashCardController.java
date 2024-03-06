280c0d300ebab5f50bd9347def2cf795c4fd9e10
https://thamudhifernando@gitlab.com <fernandothamudhi@gmail.com>
2024-03-06 16:46:10 +0530

    Create H2 in memory database and Repository

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,21 +1,36 @@
 package example.cashcard;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
 
+    @Autowired
+    private final CashCardRepository cashCardRepository;
+
+    public CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
+
     @GetMapping("/{requestedId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long requestedId){
-
-        if (requestedId.equals(99L)) {
+        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+        /*if (requestedId.equals(99L)) {
             CashCard cashCard = new CashCard(99L, 123.45);
             return ResponseEntity.ok(cashCard);
         } else {
+            return ResponseEntity.notFound().build();
+        }*/
+        if (cashCardOptional.isPresent()){
+            return ResponseEntity.ok(cashCardOptional.get());
+        }else {
             return ResponseEntity.notFound().build();
         }
     }
